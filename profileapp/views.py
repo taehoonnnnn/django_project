fd1fe62011de18d1f2ffec8c039250d375ab2982
taehoon <enter4652@naver.com>
2023-02-27 17:03:00 +0900

    django course 32 commit

--- profileapp/views.py
+++ profileapp/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 from django.urls import reverse_lazy
-from django.views.generic import CreateView
+from django.views.generic import CreateView, UpdateView
+from django.utils.decorators import method_decorator
 
+
+from profileapp.decorators import profile_ownership_required
 from profileapp.forms import ProfileCreateionForm
 from profileapp.models import Profile
 
@@ -14,6 +17,22 @@ class ProfileCreateView(CreateView):
     form_class = ProfileCreateionForm
     success_url = reverse_lazy('accountapp:hello_world')
     template_name = 'profileapp/create.html'
+
+    def form_valid(self, form):
+        temp_profile = form.save(commit=False)
+        temp_profile.user = self.request.user
+        temp_profile.save()
+        return super().form_valid(form)
+
+
+@method_decorator(profile_ownership_required, 'get')
+@method_decorator(profile_ownership_required, 'post')
+class ProfileUpdateView(UpdateView):
+    model = Profile
+    context_object_name = 'target_profile'
+    form_class = ProfileCreateionForm
+    success_url = reverse_lazy('accountapp:hello_world')
+    template_name = 'profileapp/update.html'
 
     def form_valid(self, form):
         temp_profile = form.save(commit=False)
