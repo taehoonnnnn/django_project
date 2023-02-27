2ce3c3b92cebbc263be890e21e24f219d1638977
taehoon <enter4652@naver.com>
2023-02-27 15:59:40 +0900

    django course 30 commit

--- pragmatic/settings.py
+++ pragmatic/settings.py
@@ -49,6 +49,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'bootstrap4',
     'accountapp',
+    'profileapp',
 ]
 
 MIDDLEWARE = [
--- pragmatic/urls.py
+++ pragmatic/urls.py
@@ -18,5 +18,7 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('accounts/', include('accountapp.urls'))
+    path('accounts/', include('accountapp.urls')),
+    path('profiles/', include('profileapp.urls')),
+
 ]
