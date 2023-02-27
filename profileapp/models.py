753078e462bc4967d405ea0ea2a992e2bf7958b3
taehoon <enter4652@naver.com>
2023-02-27 16:42:46 +0900

    django course 31 commit

--- profileapp/models.py
+++ profileapp/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 # Create your models here.
 
 
-class profile(models.Model):
+class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
 
     image = models.ImageField(upload_to='profile/', null=True)
