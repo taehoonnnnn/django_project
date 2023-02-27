69c296a50ca1ea0dd666de5671f48942689764dd
taehoon <enter4652@naver.com>
2023-02-27 17:13:25 +0900

    django course 33 commit

--- profileapp/views.py
+++ profileapp/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.urls import reverse_lazy
+from django.urls import reverse, reverse_lazy
 from django.views.generic import CreateView, UpdateView
 from django.utils.decorators import method_decorator
 
@@ -15,7 +15,7 @@ class ProfileCreateView(CreateView):
     model = Profile
     context_object_name = 'target_profile'
     form_class = ProfileCreateionForm
-    success_url = reverse_lazy('accountapp:hello_world')
+    # success_url = reverse_lazy('accountapp:detail')
     template_name = 'profileapp/create.html'
 
     def form_valid(self, form):
@@ -23,6 +23,9 @@ class ProfileCreateView(CreateView):
         temp_profile.user = self.request.user
         temp_profile.save()
         return super().form_valid(form)
+    
+    def get_success_url(self):
+        return reverse('accountapp:detail', kwargs={'pk': self.object.user.pk})
 
 
 @method_decorator(profile_ownership_required, 'get')
@@ -31,11 +34,7 @@ class ProfileUpdateView(UpdateView):
     model = Profile
     context_object_name = 'target_profile'
     form_class = ProfileCreateionForm
-    success_url = reverse_lazy('accountapp:hello_world')
     template_name = 'profileapp/update.html'
 
-    def form_valid(self, form):
-        temp_profile = form.save(commit=False)
-        temp_profile.user = self.request.user
-        temp_profile.save()
-        return super().form_valid(form)
+    def get_success_url(self):
+        return reverse('accountapp:detail', kwargs={'pk': self.object.user.pk})
