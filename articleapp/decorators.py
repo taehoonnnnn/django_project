bd8cace3f8646e0cf2bb0f6bf39e8c81d3d66e9b
taehoon <enter4652@naver.com>
2023-02-28 16:21:29 +0900

    django course 35 commit

---
+++ articleapp/decorators.py
@@ -0,0 +1,17 @@
+
+from django.http import HttpResponseForbidden
+
+from articleapp.models import Article
+
+def article_ownership_required(func):
+    def decorated(request, *args, **kwargs):
+        article = Article.objects.get(pk=kwargs['pk'])
+
+        print('닉네임',article.writer, request.user)
+        print(article)
+
+        if not article.writer == request.user:
+            return HttpResponseForbidden()
+        return func(request, *args, **kwargs)
+
+    return decorated
