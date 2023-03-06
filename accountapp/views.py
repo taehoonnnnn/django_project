e6f53a1fc6a90533c59c22173b6451f27ae41017
taehoon <enter4652@naver.com>
2023-03-06 13:55:28 +0900

    django course 41 commit

--- accountapp/views.py
+++ accountapp/views.py
@@ -7,10 +7,13 @@ from django.urls import reverse, reverse_lazy
 from django.views.generic import CreateView, DetailView, UpdateView, DeleteView
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+from django.views.generic.list import MultipleObjectMixin
+
 
 from accountapp.forms import AccountUpdateForm
 from accountapp.models import HelloWorld
 from accountapp.decorators import account_ownership_required
+from articleapp.models import Article
 from commentapp.froms import CommentCreationForm
 
 has_ownership = [login_required, account_ownership_required]
@@ -42,10 +45,15 @@ class AccountCreateView(CreateView):
     template_name = 'accountapp/create.html'
 
 
-class AccountDetailView(DetailView):
+class AccountDetailView(DetailView, MultipleObjectMixin):
     model = User
     context_object_name = 'target_user'
     template_name = 'accountapp/detail.html'
+    paginate_by = 25
+
+    def get_context_data(self, **kwargs):
+        object_list = Article.objects.filter(writer=self.get_object())
+        return super(AccountDetailView, self).get_context_data(object_list=object_list, **kwargs)
 
 
 @method_decorator(has_ownership, 'get')
