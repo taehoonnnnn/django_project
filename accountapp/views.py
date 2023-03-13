2e199c32f2aaebe7e27b7bb8234e2d0c67928e24
taehoon <enter4652@naver.com>
2023-03-13 16:14:15 +0900

    accountapp:create update

--- accountapp/views.py
+++ accountapp/views.py
@@ -20,7 +20,7 @@ has_ownership = [login_required, account_ownership_required]
 class AccountCreateView(CreateView):
     model = User
     form_class = UserCreationForm
-    success_url = reverse_lazy('accountapp:hello_world')
+    success_url = reverse_lazy('articleapp:list')
     template_name = 'accountapp/create.html'
 
 
@@ -41,7 +41,7 @@ class AccountUpdateView(UpdateView):
     model = User
     context_object_name = 'target_user'
     form_class = AccountUpdateForm
-    success_url = reverse_lazy('accountapp:hello_world')
+    success_url = reverse_lazy('articleapp:list')
     template_name = 'accountapp/update.html'
 
 
