62306bcdb24fc91aaf5e60d6da94ccc118f20b61
taehoon <enter4652@naver.com>
2023-03-14 15:13:17 +0900

    docker secret_v2

--- pragmatic/settings/deploy.py
+++ pragmatic/settings/deploy.py
@@ -21,7 +21,7 @@ environ.Env.read_env(env_file = os.path.join(BASE_DIR, '.env'))
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = read_secret('DAJNGO_SECRET_KEY')
+SECRET_KEY = read_secret('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
