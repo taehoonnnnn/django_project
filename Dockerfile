0326268120fb7fdf83587de4a9ef7127cbfb8650
taehoon <enter4652@naver.com>
2023-03-20 16:16:53 +0900

    django makeup course 12

--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,7 @@ FROM python:3.9.0
 
 WORKDIR /home/
 
-RUN echo "testing4"
+RUN echo "testing5"
 
 RUN git clone https://github.com/taehoonnnnn/django_project.git
 
--- likeapp/views.py
+++ likeapp/views.py
@@ -1,15 +1,28 @@
 from django.contrib.auth.decorators import login_required
+from django.forms import ValidationError
 from django.http import HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
 from django.utils.decorators import method_decorator
 from django.views.generic import RedirectView
-
 from django.contrib import messages
-
+from django.db import transaction
+# Create your views here.
 from articleapp.models import Article
 from likeapp.models import LikeRecord
-# Create your views here.
+
+
+@transaction.atomic
+def db_transaction(user, article):
+
+    if LikeRecord.objects.filter(user=user, article=article).exists():
+        raise ValidationError('Like already exists')
+    else:
+        LikeRecord(user=user, article=article).save()
+
+    article.like += 1
+    article.save()
+
 
 @method_decorator(login_required, 'get')
 class LikeArticleView(RedirectView):
@@ -21,15 +34,11 @@ class LikeArticleView(RedirectView):
         user = self.request.user
         article = get_object_or_404(Article, pk=kwargs['pk'])
 
-        if LikeRecord.objects.filter(user=user, article=article).exists():
+        try:
+            db_transaction(user, article)
+            messages.add_message(self.request, messages.SUCCESS, '좋아요가 반영되었습니다.')
+        except ValidationError:
             messages.add_message(self.request, messages.ERROR, '좋아요는 한번만 가능합니다.')
-            return HttpResponseRedirect(reverse('articleapp:detail', kwargs={'pk': kwargs['pk']}))
-        else:
-            LikeRecord(user=user, article=article).save()
-
-        article.like += 1
-        article.save()
-
-        messages.add_message(self.request, messages.SUCCESS, '좋아요가 반영되었습니다.')
-
+            return HttpResponseRedirect(reverse('articleapp:detail', kwargs={'pk': article.pk}))
+        
         return super(LikeArticleView, self).get(self.request, *args, **kwargs)
